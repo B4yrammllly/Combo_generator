fd48392fa01d4d55286b0bbc631b5da743077abc
B4yrammllly <109522372+B4yrammllly@users.noreply.github.com>
2022-08-01 16:17:09 +0300

    Add files via upload

---
+++ ccombogenerator.py
@@ -0,0 +1,54 @@
+import os
+from random import *
+os.system("clear")
+print("**************************************************************\nB4YRAMLLLY CC - MAİL - PASSWORD GENERATOR\n**************************************************************\n\n")
+
+print("TOOL HOLD = B4YRAMLLY \nİNSTAGRAM = b4yramllly\nTELEGRAM =DarKkFACE\nCONTACT = yasuomainim6@gmail.com\n\n")
+print("DİĞER TOOLLARIM(LİNKLERİ AÇARAK BANA DESTEK OLABİLİRSİNİZ)\n*****************************************\nhttps://ay.live/kCyq5k\nhttps://ay.link/CplF\n*****************************************")
+print("İŞLEMLER 👇👇\n")
+print("""
+{1}CC OLUŞTURUCU\n
+{2}MAİL + PASSWORD OLUŞTURUCU\n
+{3}PASSWORD oluşturucu\n\n
+""")
+
+mal = int(input("SEÇMEK İSTEDİĞİNİZ İŞLEM NO : "))
+
+hayırlısı=['_','1','2','3','4','5','6','7','8'  ,'9'  ,'0','qwe','']
+
+lan = "@gmail.com : "
+
+b = "|"
+a=['6','5','4','1','2','3', '7', '8', '9' , '0']
+ü=['2023', '2024', '2025' ]
+d=['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+
+
+ş =['a','v','e','r','t','y','u','ı','i','o','p','s','d','f','g','h','k','l','z','c','b','n','m','ö']
+if(mal == 1):
+	k=int(input("KAÇ CC OLUŞTURULSUN : "))
+	print("\n")
+	for i in range(k):
+	  	print(a[randint(0,9)]+a[randint(0,9)]+a[randint(0,9)]+a[randint(0,9)]+a[randint(0,9)]+a[randint(0,9)]+a[randint(0,9)] +a[randint(0,9)]+a[randint(0,9)]+a[randint(0,9)]+a[randint(0,9)]+a[randint(0,9)]+a[randint(0,9)]+a[randint(0,9)]+a[randint(0,9)]+a[randint(0,9)]+a[randint(0,9)] +b +d[randint(0,11)] +b +ü[randint(0,2)] +b +a[randint(0,9)] +a[randint(0,9)] +a[randint(0,9)]
+	  	
+	  	)
+
+   	
+elif(mal == 2):
+	o=int(input("KAÇ MAİL : PASS OLUŞSUN : "))
+	print("\n")	
+	for i in range(o):
+		print(ş[randint(0,23)]+ş[randint(0,23)] +ş[randint(0,23)]+ş[randint(0,23)]+ş[randint(0,23)]+ş[randint(0,23)]+ş[randint(0,23)]+ş[randint(0,23)] +ş[randint(0,23)]+ş[randint(0,23)]+ş[randint(0,23)]+ş[randint(0,23)]+ş[randint(0,23)]+lan +ş[randint(0,23)] +ş[randint(0,23)]+ş[randint(0,23)]+ş[randint(0,23)]+ş[randint(0,23)]+ş[randint(0,23)]+ş[randint(0,23)] +ş[randint(0,23)]+ş[randint(0,23)]
+		
+		)
+	
+	
+elif(mal == 3 ):
+		q = int(input("KAÇ ŞİFRE OLUŞTURULSUN : "))	
+		print("\n")
+		kul = input("kullanıcı adı giriniz")
+		for i in range(q):
+			print( kul+hayırlısı[randint(0,11)]+hayırlısı[randint(0,11)]+hayırlısı[randint(0,11)])
+			
+else:
+	print("GEÇERSİZ İŞLEM...TEKRAR ÇALIŞTIRINIZ")
